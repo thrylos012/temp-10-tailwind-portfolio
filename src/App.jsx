dac25ec67c2d7bc835e39753eb2511e2cebb94d0
balafoutisp <thrylos012@gmail.com>
2023-10-26 16:35:23 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import About from "./components/About";
+import Projects from "./components/Projects";
+
+const App = () => {
+    return (
+        <>
+            <Navbar />
+            <Hero />
+            <Skills />
+            <About />
+            <Projects />
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/Projects.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SectionTitle from "./SectionTitle";
+import { projects } from "../data";
+import ProjectsCard from "./ProjectsCard";
+
+const Projects = () => {
+    return (
+        <section className="py-20 align-element" id="projects">
+            <SectionTitle text="web creations" />
+            <div className="py-16 grid lg:grid-cols-2 xl:grid-cols-3 gap-8">
+                {projects.map((project) => {
+                    return <ProjectsCard key={project.id} {...project} />;
+                })}
+            </div>
+        </section>
+    );
+};
+
+export default Projects;
